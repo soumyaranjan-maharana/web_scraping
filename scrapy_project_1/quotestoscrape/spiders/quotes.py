43abe99ff41a70dad4bd6e605f45e54d0ee9f0c7
soumya <soumyaranjan.maharana066@gmail.com>
2026-09-25 21:08:57 +0530

    add a scrapy project

---
+++ scrapy_project_1/quotestoscrape/spiders/quotes.py
@@ -0,0 +1,25 @@
+import scrapy
+from ..items import QuotestoscrapeItem
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = ["https://quotes.toscrape.com/"]
+
+    def parse(self, response):
+        for quote in response.css(".quote"):
+            items = QuotestoscrapeItem()
+
+            title = quote.css(".text::text").get()
+            author = quote.css(".author::text").get()
+            tag = quote.css(".tags .tag::text").getall()
+
+            items.title = title
+            items.author = author
+            items.tag = tag
+
+            yield items
+
+        nxt_page = response.css(".next a::attr(href)").get()
+        if nxt_page is not None:
+            yield response.follow(nxt_page, self.parse)
